e9d9461030ca8b5a5429ceeec804ab0251a2fbac
Jeremy <Jeremyanthonydegroot@gmail.com>
2023-04-26 09:25:15 +0200

    add functionality for 2 teams

--- scavenger_hunt.py
+++ scavenger_hunt.py
@@ -12,14 +12,12 @@ def guess_password(object_codes_list):
     print("""
 Congratulations on making it this far!
 You've entered every code and now you have to guess the password.
-Read carefully!
 
-You can create the password by combining every code you've entered.
-The password is a sentence and the spaces are replaced by underscores('_').""")
+You can create the password by combining every code you've entered in a logical order""")
 
     while True:
         password_input = input("Enter the password: ")
-        if password_input == sha.password_input:
+        if password_input == sha.t1_password_input or password_input == sha.t2_password_input:
             victory()
             exit()
         else:
@@ -34,14 +32,17 @@ def scavenger_hunt_main():
     print("Welcome to the scavenger hunt!")
     print_game_rules()
 
-    object_codes_list = []
+    t1_object_codes_list = []
+    t2_object_codes_list = []
     while True:
         try:
             menu_input = int(input("""
 1. Show rules again
-2. Add code to the list
-3. Show obtained codes
-4. Attempt to guess the password
+2. [Team 1] Add code to the list
+3. [Team 1] Show obtained codes
+4. [Team 2] Add code to the list
+5. [Team 2] Show obtained codes
+6. Attempt to guess the password
 Please select an option: """))
         
         except:
@@ -52,41 +53,61 @@ Please select an option: """))
 
         elif menu_input == 2:
             while True:
-                add_code = input("\nEnter the code written on the item you've hunted (enter 'exit' to return to the menu): ")
+                t1_add_code = input("\nEnter the code written on the item you've hunted (enter 'exit' to return to the menu): ")
 
-                if add_code.lower() == "exit":
+                if t1_add_code.lower() == "exit":
                     break
 
-                if add_code ==  sha.add_code_1 or add_code == sha.add_code_2 or add_code == sha.add_code_3 or add_code == sha.add_code_4:
-                    if add_code in object_codes_list:
+                if t1_add_code ==  sha.t1_add_code_1 or t1_add_code == sha.t1_add_code_2 or t1_add_code == sha.t1_add_code_3 or t1_add_code == sha.t1_add_code_4:
+                    if t1_add_code in t1_object_codes_list:
                         print("\nThe code has already been added to the list!")
                         pass
-                    object_codes_list.append(add_code)
-                    print_obtained_codes = print(f"\nThese are the codes you've obtained so far: {object_codes_list}")
+                    t1_object_codes_list.append(t1_add_code)
+                    print_obtained_codes = print(f"\nThese are the codes you've obtained so far: {t1_object_codes_list}")
                     
-                    if len(object_codes_list) == 4:
-                        print("\nCongratulations, you've obtained and entered every code to the list, you are now able to guess the password!")
+                    if len(t1_object_codes_list) == 4:
+                        print("\nCongratulations Team 1, you've obtained and entered every code to the list, you are now able to guess the password!")
                         break
 
-                    # add_code_again = input("Do you want to enter another code? [y/n] ")
-                    
-                    # if add_code_again.lower() == "y":
-                    #     continue
-                    # elif add_code_again.lower() == "n":
-                    #     break
-                    # else:
-                    #     print("Please enter a valid input!")
                 else:
-                    print("\nPlease correctly enter the code written on the object!")
+                    print("\nPlease correctly enter the code written on the object! (case sensitive)")
 
         elif menu_input == 3:
-            oprint_obtained_codes = print(f"These are the codes you've obtained so far: {object_codes_list}")
-            if len(object_codes_list) == 4:
-                print("\nCongratulations, you've obtained and entered every code to the list, you are now able to guess the password!")
+            oprint_obtained_codes = print(f"These are the codes you've obtained so far: {t1_object_codes_list}")
+            if len(t1_object_codes_list) == 4:
+                print("\nCongratulations Team 1, you've obtained and entered every code to the list, you are now able to guess the password!")
         
         elif menu_input == 4:
-            if len(object_codes_list) == 4:
-                guess_password(object_codes_list)
+            while True:
+                t2_add_code = input("\nEnter the code written on the item you've hunted (enter 'exit' to return to the menu): ")
+
+                if t2_add_code.lower() == "exit":
+                    break
+
+                if t2_add_code ==  sha.t2_add_code_1 or t2_add_code == sha.t2_add_code_2 or t2_add_code == sha.t2_add_code_3 or t2_add_code == sha.t2_add_code_4:
+                    if t2_add_code in t2_object_codes_list:
+                        print("\nThe code has already been added to the list!")
+                        pass
+                    t2_object_codes_list.append(t2_add_code)
+                    print_obtained_codes = print(f"\nThese are the codes you've obtained so far: {t2_object_codes_list}")
+                    
+                    if len(t2_object_codes_list) == 4:
+                        print("\nCongratulations Team 2, you've obtained and entered every code to the list, you are now able to guess the password!")
+                        break
+
+                else:
+                    print("\nPlease correctly enter the code written on the object! (case sensitive)")
+
+        elif menu_input == 5:
+            oprint_obtained_codes = print(f"These are the codes you've obtained so far: {t2_object_codes_list}")
+            if len(t2_object_codes_list) == 4:
+                print("\nCongratulations Team 2, you've obtained and entered every code to the list, you are now able to guess the password!")
+
+        elif menu_input == 6:
+            if len(t1_object_codes_list) == 4:
+                guess_password(t1_object_codes_list)
+            elif len(t2_object_codes_list) == 4:
+                guess_password(t2_object_codes_list)
             else:
-                print("\nThe list isn't complete yet, please add all the codes to the list!")
+                print("\nNone of the lists are complete yet, please add all the codes to your team's list!")
 scavenger_hunt_main()
